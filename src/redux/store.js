4cbd3534e8b4c3a08beeaf2c4299a5b70be8a58b
Greg Stickel <gregory.stickel@live.com>
2022-12-06 11:30:54 -0500

    Updated 11:30pm 12-06-22

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import potentialCountriesReducer from '../redux/slices/potentialCountriesSlice';
+import displayCountryReducer from "../redux/slices/displayCountrySlice";
+
+
+export default configureStore({
+    reducer: { 
+        potentialCountries: potentialCountriesReducer,
+        displayedCountry: displayCountryReducer,
+    },
+});
